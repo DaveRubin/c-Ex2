27764166b04d127411d992a39fe0a89fb935cf4c
barvazos <amitaihandler@gmail.com>
2016-05-01 23:16:47 +0300

    IsGameWon Bug fixed
    didn't iterate through all of the columns (upper bound was too low)

--- B16_Ex02/BoardAnalyzer.cs
+++ B16_Ex02/BoardAnalyzer.cs
@@ -8,11 +8,10 @@ namespace B16_Ex02
     {
         internal static bool IsGameWon(ref Board i_Board)
         {
-
             /// Vertical search for winner
             for (int row = 0; row < i_Board.r_numOfRows - 3; row++)
             {
-                for (int col = 0; col < i_Board.r_numOfColumns - 1; col++)
+                for (int col = 0; col < i_Board.r_numOfColumns; col++)
                 {
                     if (i_Board.m_slotsMatrix[col, row] != Board.eSlotState.Empty &&
                     i_Board.m_slotsMatrix[col, row] == i_Board.m_slotsMatrix[col, row + 1] &&
@@ -23,7 +22,6 @@ namespace B16_Ex02
                     }
                 }
             }
-
             /// Horizontal search for winner
             for (int row = 0; row < i_Board.r_numOfRows; row++)
             {
@@ -38,7 +36,6 @@ namespace B16_Ex02
                     }
                 } 
             }
-
             /// Right and down diagonal search for winner
             for (int row = 0; row < i_Board.r_numOfRows - 3; row++)
             {
@@ -53,7 +50,6 @@ namespace B16_Ex02
                     }
                 }
             }
-
             /// Right and up diagonal search for winner
             for (int row = 3; row < i_Board.r_numOfRows; row++)
             {
@@ -71,20 +67,22 @@ namespace B16_Ex02
             return false;
         }
 
-        
-
-        internal static bool IsWinnningMove(ref Board i_Board, int i_col, Board.eSlotState i_pieceType)
+        internal static bool IsWinnningMove(ref Board i_board, int i_col, Board.eSlotState i_pieceType)
         {
-            if (i_Board.AddPieceToColumn(i_col,i_pieceType))
+            if (i_board.AddPieceToColumn(i_col, i_pieceType))
             {
-                return IsGameWon(ref i_Board);
+                if (IsGameWon(ref i_board))
+                {
+                    i_board.RemovePieceFromColumn(i_col);
+                    return true;
+                }
+                else
+                {
+                    i_board.RemovePieceFromColumn(i_col);
+                    return false;
+                }
             }
-            else
-            {
-                return false;
-            }
+            return false;
         }
-
-
     }
 }
